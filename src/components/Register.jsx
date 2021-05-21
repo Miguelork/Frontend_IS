74e5d2ab3adca4e5c8550c42f56db83318d991dd
Manuel_Rodriguez <manuelrodriguez@correo.unimet.edu.ve>
2021-05-20 20:31:39 -0400

    Mejorando la vista de Registro

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,55 +1,229 @@
-import { render } from '@testing-library/react';
 import React from 'react'
-import './css/Register.css';
+import Header from './Header'
 
-class Register extends React.Component {
+// onChange={(e) => mostrarDiv(e.target.value)} <------ NO BORRAR
 
-
-    handleSubmit(event) {
-        alert("Submit presionado");
-    }
-
-    render(){
-        return (
-            <div class="container register-form">
-                <h1>Registro</h1>
-            <div class="form">
-                <div class="form-content">
-                    <form onSubmit={this.handleSubmit}>
-                        <div class="row justify-content-center">
-                            <div class="col-md-6">
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Nombre" value=""/>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Numero de telefono" value=""/>
-                                </div>
-                                <div class="form-group">
-                                    <input type="email" class="form-control" placeholder="Correo Electronico" value=""/>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Direccion" value=""/>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Nombre de usuario" value=""/>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Contraseña" value=""/>
-                                </div>
-                            </div>
-                        </div>
-                        <button type="submit" class="btnSubmit">Submit</button>
-                    </form>
-                </div>
-            </div>
-            </div>
-        );
-    }
-    
-        
-    
+function mostrarDivDoctor() {
+    document.getElementById('pricing').style.display = 'none';
+    document.getElementById('divDoctor').style.display = '';
 }
 
+function mostrarDivPaciente() {
+    document.getElementById('pricing').style.display = 'none';
+    document.getElementById('divPaciente').style.display = '';
+}
 
+function registrarDoctor() {
+    var Doctor = new Object();
+    Doctor.nombre = document.getElementById("nombreD").value;
+    Doctor.apellido = document.getElementById("apellidoD").value;
+    Doctor.user = document.getElementById("userD").value;
+    Doctor.password = document.getElementById("passwordD").value;
+    Doctor.telefono = document.getElementById("telefonoD").value;
+    Doctor.email = document.getElementById("emailD").value;
+    Doctor.direccion = document.getElementById("direccionD").value;
+    Doctor.especialidades = document.getElementById("especialidadesD").value;
+    Doctor.hora = document.getElementById("horaD").value;
+    console.log(Doctor)
+}  
+
+function registrarPaciente() {
+    var Paciente = new Object();
+    Paciente.nombre = document.getElementById("nombreP").value;
+    Paciente.apellido = document.getElementById("apellidoP").value;
+    Paciente.user = document.getElementById("userP").value;
+    Paciente.password = document.getElementById("passwordP").value;
+    Paciente.telefono = document.getElementById("telefonoP").value;
+    Paciente.email = document.getElementById("emailP").value;
+    Paciente.direccion = document.getElementById("direccionP").value;
+    console.log(Paciente)
+}  
+
+const Register = () => {
+    return (
+<div>
+   <Header></Header>
+   {/* ======= Hero Section ======= */}
+   <section id="hero">
+      <section id="pricing" className="pricing" style={{"padding":"1rem"}}>
+         <div className="container">
+            <div className="row" data-aos="fade-center" style={{"justify-content":"center"}}>
+               <div className="col-lg-3 col-md-6">
+                  <div className="box" data-aos="zoom-in" data-aos-delay={100}>
+                     <h3>Free</h3>
+                     <h4><sup>$</sup>0<span> / month</span></h4>
+                     <ul>
+                        <li>Aida dere</li>
+                        <li>Nec feugiat nisl</li>
+                        <li>Nulla at volutpat dola</li>
+                        <li className="na">Pharetra massa</li>
+                        <li className="na">Massa ultricies mi</li>
+                     </ul>
+                     <div className="btn-wrap">
+                        <a href="#" className="btn-buy" onClick={ mostrarDivPaciente }>Buy Now</a>
+                     </div>
+                  </div>
+               </div>
+               <div className="col-lg-3 col-md-6 mt-4 mt-md-0">
+                  <div className="box featured" data-aos="zoom-in" data-aos-delay={200}>
+                     <h3>Business</h3>
+                     <h4><sup>$</sup>19<span> / month</span></h4>
+                     <ul>
+                        <li>Aida dere</li>
+                        <li>Nec feugiat nisl</li>
+                        <li>Nulla at volutpat dola</li>
+                        <li>Pharetra massa</li>
+                        <li className="na">Massa ultricies mi</li>
+                     </ul>
+                     <div className="btn-wrap">
+                        <a href="#" className="btn-buy" onClick={ mostrarDivDoctor }>Buy Now</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      {/* End Pricing Section */}
+      <div>
+      </div>
+      <div id="divDoctor" style={{"display":"none"}}>
+         <section id="features" className="features" style={{"padding":"1rem"}}>
+            <div className="container">
+               <div className="section-title" data-aos="fade-up">
+                  <h2>Registro</h2>
+                  <p style={{"color":"white"}}>Doctor</p>
+               </div>
+               <div className="row" data-aos="fade-left">
+               <form role="form" class="php-email-form" style={{"width":"100%"}}>
+                     <div class="form-row">
+                        <div class="col-md-6 form-group">
+                           <input type="text" class="form-control" id="nombreD" placeholder="Escriba su Nombre" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                           <div class="validate"></div>
+                        </div>
+                        <div class="col-md-6 form-group">
+                           <input type="text" class="form-control" id="apellidoD" placeholder="Escriba su Apellido" data-rule="email" data-msg="Please enter a valid email" />
+                           <div class="validate"></div>
+                        </div>
+                     </div>
+                     <div class="form-row">
+                        <div class="col-md-6 form-group">
+                           <input type="text" class="form-control" id="userD" placeholder="Escriba su Usuario" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                           <div class="validate"></div>
+                        </div>
+                        <div class="col-md-6 form-group">
+                           <input type="password" class="form-control" id="passwordD" placeholder="Escriba su Contraseña" data-rule="email" data-msg="Please enter a valid email" />
+                           <div class="validate"></div>
+                        </div>
+                     </div>                     
+                     <div class="form-row">
+                        <div class="col-md-6 form-group">
+                           <input type="text" class="form-control" id="telefonoD" placeholder="Escriba su Teléfono " data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                           <div class="validate"></div>
+                        </div>
+                        <div class="col-md-6 form-group">
+                           <input type="email" class="form-control" id="emailD" placeholder="Escriba su Email" data-rule="email" data-msg="Please enter a valid email" />
+                           <div class="validate"></div>
+                        </div>
+                     </div>
+                     <div class="form-group">
+                           <input type="text" class="form-control" id="direccionD" placeholder="Escriba su Dirección" data-rule="email" data-msg="Please enter a valid email" />
+                           <div class="validate"></div>
+                        </div>
+                        <div class="form-row">
+                        <div class="col-md-6 form-group">
+                           <input type="text" class="form-control" id="especialidadesD" placeholder="Escriba su Especialidades" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                           <div class="validate"></div>
+                        </div>
+                        <div class="col-md-6 form-group">
+                           <input type="text" class="form-control" id="horaD" placeholder="Escriba su Hora de atención. Ej: 8:00AM a 11:00AM" data-rule="email" data-msg="Please enter a valid email" />
+                           <div class="validate"></div>
+                        </div>
+                     </div>
+                     <div class="mb-3">
+                        <div style={{"display":"none" , "color":"white"}}>¡Ha ocurrido un error!</div>
+                     </div>
+                     <div class="text-center"><a href="#" onClick={ registrarDoctor } className="btn-get-started scrollto">Aceptar</a></div>
+                  </form>
+               </div>
+            </div>
+         </section>
+         {/* End Features Section */}
+      </div>
+      <div id="divPaciente" style={{"display":"none"}}>
+         <section id="features" className="features" style={{"padding":"1rem"}}>
+            <div className="container">
+               <div className="section-title" data-aos="fade-up">
+                  <h2>Registro</h2>
+                  <p style={{"color":"white"}}>Paciente</p>
+               </div>
+               <div className="row" data-aos="fade-left">
+                  <form role="form" class="php-email-form" style={{"width":"100%"}}>
+                     <div class="form-row">
+                        <div class="col-md-6 form-group">
+                           <input type="text" class="form-control" id="nombreP" placeholder="Escriba su Nombre" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                           <div class="validate"></div>
+                        </div>
+                        <div class="col-md-6 form-group">
+                           <input type="text" class="form-control" id="apellidoP" placeholder="Escriba su Apellido" data-rule="email" data-msg="Please enter a valid email" />
+                           <div class="validate"></div>
+                        </div>
+                     </div>
+                     <div class="form-row">
+                        <div class="col-md-6 form-group">
+                           <input type="text" class="form-control" id="userP" placeholder="Escriba su Usuario" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                           <div class="validate"></div>
+                        </div>
+                        <div class="col-md-6 form-group">
+                           <input type="password" class="form-control" id="passwordP" placeholder="Escriba su Contraseña" data-rule="email" data-msg="Please enter a valid email" />
+                           <div class="validate"></div>
+                        </div>
+                     </div>                     
+                     <div class="form-row">
+                        <div class="col-md-6 form-group">
+                           <input type="text" class="form-control" id="telefonoP" placeholder="Escriba su Teléfono " data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                           <div class="validate"></div>
+                        </div>
+                        <div class="col-md-6 form-group">
+                           <input type="email" class="form-control" id="emailP" placeholder="Escriba su Email" data-rule="email" data-msg="Please enter a valid email" />
+                           <div class="validate"></div>
+                        </div>
+                     </div>
+                     <div class="form-group">
+                           <input type="text" class="form-control" id="direccionP" placeholder="Escriba su Dirección" data-rule="email" data-msg="Please enter a valid email" />
+                           <div class="validate"></div>
+                        </div>
+                     <div class="mb-3">
+                        <div style={{"display":"none" , "color":"white"}}>¡Ha ocurrido un error!</div>
+                     </div>
+                     <div class="text-center"><a href="#" onClick={ registrarPaciente }  className="btn-get-started scrollto">Aceptar</a></div>
+                  </form>
+               </div>
+            </div>
+         </section>
+         {/* End Features Section */}
+      </div>
+      <svg className="hero-waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
+         <defs>
+            <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
+            </path>
+         </defs>
+         <g className="wave1">
+            <use xlinkHref="#wave-path" x={50} y={3} fill="rgba(255,255,255, .1)">
+            </use>
+         </g>
+         <g className="wave2">
+            <use xlinkHref="#wave-path" x={50} y={0} fill="rgba(255,255,255, .2)">
+            </use>
+         </g>
+         <g className="wave3">
+            <use xlinkHref="#wave-path" x={50} y={9} fill="#fff">
+            </use>
+         </g>
+      </svg>
+   </section>
+   {/* End Hero */}
+</div>
+    )
+}
 
 export default Register
