62c13d061e94b462e9ce624fe7e0e9af44b98744
Manuel_Rodriguez <manuelrodriguez@correo.unimet.edu.ve>
2021-05-26 11:56:42 -0400

    Detalle urgente en registros

--- src/components/FormDoctor.jsx
+++ src/components/FormDoctor.jsx
@@ -87,14 +87,14 @@ const FormDoctor = () => {
                 <h2>Registro</h2>
                 <p style={{ "color": "white" }}>Doctor</p>
             </div>
-            <div className="row" data-aos="fade-left" id="finalizando"  style={{ "display": "none" }}>  
+            <div className="row" data-aos="fade-left" id="DocFinalizando"  style={{ "display": "none" }}>  
                 <div className="lds-spinner" style={{ "padding-right": "90px" }} ><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /></div>
                 <p style={{ "color": "white", "font-size":"12px"}}>
                     Se esta registrando con exito <br /> Sera redirigido al Login para que inicie sesión
                 </p>
                 <style dangerouslySetInnerHTML={{__html: "\n.lds-spinner {\n  color: official;\n  display: inline-block;\n  position: relative;\n  width: 80px;\n  height: 80px;\n}\n.lds-spinner div {\n  transform-origin: 40px 40px;\n  animation: lds-spinner 1.2s linear infinite;\n}\n.lds-spinner div:after {\n  content: \" \";\n  display: block;\n  position: absolute;\n  top: 3px;\n  left: 37px;\n  width: 6px;\n  height: 18px;\n  border-radius: 20%;\n  background: #fff;\n}\n.lds-spinner div:nth-child(1) {\n  transform: rotate(0deg);\n  animation-delay: -1.1s;\n}\n.lds-spinner div:nth-child(2) {\n  transform: rotate(30deg);\n  animation-delay: -1s;\n}\n.lds-spinner div:nth-child(3) {\n  transform: rotate(60deg);\n  animation-delay: -0.9s;\n}\n.lds-spinner div:nth-child(4) {\n  transform: rotate(90deg);\n  animation-delay: -0.8s;\n}\n.lds-spinner div:nth-child(5) {\n  transform: rotate(120deg);\n  animation-delay: -0.7s;\n}\n.lds-spinner div:nth-child(6) {\n  transform: rotate(150deg);\n  animation-delay: -0.6s;\n}\n.lds-spinner div:nth-child(7) {\n  transform: rotate(180deg);\n  animation-delay: -0.5s;\n}\n.lds-spinner div:nth-child(8) {\n  transform: rotate(210deg);\n  animation-delay: -0.4s;\n}\n.lds-spinner div:nth-child(9) {\n  transform: rotate(240deg);\n  animation-delay: -0.3s;\n}\n.lds-spinner div:nth-child(10) {\n  transform: rotate(270deg);\n  animation-delay: -0.2s;\n}\n.lds-spinner div:nth-child(11) {\n  transform: rotate(300deg);\n  animation-delay: -0.1s;\n}\n.lds-spinner div:nth-child(12) {\n  transform: rotate(330deg);\n  animation-delay: 0s;\n}\n@keyframes lds-spinner {\n  0% {\n    opacity: 1;\n  }\n  100% {\n    opacity: 0;\n  }\n}\n" }} />    
             </div>
-            <div className="row" data-aos="fade-left" id="formulario">
+            <div className="row" data-aos="fade-left" id="DocFormulario">
                 <form role="form" id="regDoctor" class="php-email-form" style={{ "width": "100%" }}>
                     <input type="hidden" id="tipoD" />
                     <div class="form-row">
@@ -259,6 +259,8 @@ async function registrarDoctor() {
 
     // Cuando este false es decir que no existe se guarde en DB y de lo contrario se muestra un alerta
     if (controlExiste == false) {
+        ocultar('DocFormulario');
+        mostrar('DocFinalizando');
     // Envio POST al backend
     axios.post('https://dblinkmed.herokuapp.com/crearUsuario', {
         "tipo": Doctor.tipo,
@@ -275,9 +277,7 @@ async function registrarDoctor() {
     })
         .then(function (response) {
             console.log(response);
-            ocultar('formulario');
-            mostrar('finalizando');
-            setTimeout(() => { window.location.href = '/login'; }, 5000);
+            setTimeout(() => { window.location.href = '/login'; }, 3000);
         })
         .catch(function (error) {
             console.log(error);
@@ -292,9 +292,10 @@ function ocultar( id ){
 }
 
 function mostrar( id ){
+    setTimeout(() => { 
     document.getElementById(id).style.display = 'block';
     document.getElementById(id).style.opacity = '100';
+    }, 500);
 }
-
 
 export default FormDoctor
