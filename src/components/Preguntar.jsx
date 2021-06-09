5af7d9e5679c0b266ca81628baae056fc10040d4
Mariavalen <maria.soto@correo.unimet.edu.ve>
2021-06-08 20:07:34 -0400

    haciendo el foro y falta pasar parametros el ID de la pregunta para seguir haciendo lo demás

--- src/components/Preguntar.jsx
+++ src/components/Preguntar.jsx
@@ -41,6 +41,11 @@ class Preguntar extends React.Component {
                                 <h2>Realizar una</h2>
                                 <p style={{ "color": "white" }}>Pregunta</p>
                             </div>
+                            <div className="row section-title" data-aos="fade-left" id="cargando" style={{ "display": "none" }}>
+                                <div className="lds-spinner" style={{ "padding-right": "90px" }} ><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /></div>
+                                <p style={{ "color": "white", "font-size": "12px" }}>Cargando</p>
+                                <style dangerouslySetInnerHTML={{ __html: "\n.lds-spinner {\n  color: official;\n  display: inline-block;\n  position: relative;\n  width: 80px;\n  height: 80px;\n}\n.lds-spinner div {\n  transform-origin: 40px 40px;\n  animation: lds-spinner 1.2s linear infinite;\n}\n.lds-spinner div:after {\n  content: \" \";\n  display: block;\n  position: absolute;\n  top: 3px;\n  left: 37px;\n  width: 6px;\n  height: 18px;\n  border-radius: 20%;\n  background: #fff;\n}\n.lds-spinner div:nth-child(1) {\n  transform: rotate(0deg);\n  animation-delay: -1.1s;\n}\n.lds-spinner div:nth-child(2) {\n  transform: rotate(30deg);\n  animation-delay: -1s;\n}\n.lds-spinner div:nth-child(3) {\n  transform: rotate(60deg);\n  animation-delay: -0.9s;\n}\n.lds-spinner div:nth-child(4) {\n  transform: rotate(90deg);\n  animation-delay: -0.8s;\n}\n.lds-spinner div:nth-child(5) {\n  transform: rotate(120deg);\n  animation-delay: -0.7s;\n}\n.lds-spinner div:nth-child(6) {\n  transform: rotate(150deg);\n  animation-delay: -0.6s;\n}\n.lds-spinner div:nth-child(7) {\n  transform: rotate(180deg);\n  animation-delay: -0.5s;\n}\n.lds-spinner div:nth-child(8) {\n  transform: rotate(210deg);\n  animation-delay: -0.4s;\n}\n.lds-spinner div:nth-child(9) {\n  transform: rotate(240deg);\n  animation-delay: -0.3s;\n}\n.lds-spinner div:nth-child(10) {\n  transform: rotate(270deg);\n  animation-delay: -0.2s;\n}\n.lds-spinner div:nth-child(11) {\n  transform: rotate(300deg);\n  animation-delay: -0.1s;\n}\n.lds-spinner div:nth-child(12) {\n  transform: rotate(330deg);\n  animation-delay: 0s;\n}\n@keyframes lds-spinner {\n  0% {\n    opacity: 1;\n  }\n  100% {\n    opacity: 0;\n  }\n}\n" }} />
+                            </div>
                             <div className="row" data-aos="fade-left">
                                 <section id="faq" className="faq section-bg" style={{ "background": "none", "padding-top": "0rem" }}>
 
@@ -72,7 +77,7 @@ class Preguntar extends React.Component {
                                             <div class="mb-3">
                                                 <div style={{ "display": "none", "color": "white" }}>¡Ha ocurrido un error!</div>
                                             </div>
-                                            <div class="text-center"><a href="#" className="btn-get-started scrollto">Aceptar</a></div>
+                                            <div class="text-center"><a href="#" onClick={ crearPregunta } className="btn-get-started scrollto">Aceptar</a></div>
                                         </form>
                                     </div>
 
@@ -85,6 +90,50 @@ class Preguntar extends React.Component {
             </div>
         )
     }
+}
+
+async function crearPregunta() {
+    var Pregunta = new Object(); // Creando el objeto pregunta
+    Pregunta.titulo = document.getElementById("titulo").value; // Obteniendo el titulo
+    Pregunta.descripcion = document.getElementById("descripcion").value; // Obteniendo la descripcion
+    let usuario = await cookies.get("usuario"); // Obteniendo los datos del usuario en cookies
+    Pregunta.nombre = usuario.nombre; // Poniendo al objeto pregunta el nombre
+    Pregunta.apellido = usuario.apellido; // Poniendo al objeto pregunta el apellido
+    Pregunta.user = usuario.user; // Poniendo la objeto pregunta el user
+    // Mostrar cargando mientrar se ejecutan toda la funcion
+    ocultar('pregunta');
+    mostrar('cargando');
+    // Envio POST al backend
+    axios.post('https://dblinkmed.herokuapp.com/crearPregunta', {
+        titulo: Pregunta.titulo,
+        descripcion: Pregunta.descripcion,
+        nombre: Pregunta.nombre,
+        apellido: Pregunta.apellido,
+        user: Pregunta.user
+    })
+        .then(function (response) {
+            console.log(response);
+            setTimeout(() => { window.location.href = '/foro'; }, 3000); // Re
+        })
+        .catch(function (error) {
+            console.log(error);
+            ocultar('cargando');
+            mostrar('pregunta');
+        });
+
+    function ocultar(id) {
+        document.getElementById(id).style.opacity = '0';
+        document.getElementById(id).style.transition = 'opacity 0.5s';
+        setTimeout(() => { document.getElementById(id).style.display = 'none'; }, 500);
+    }
+
+    function mostrar(id) {
+        setTimeout(() => {
+            document.getElementById(id).style.display = 'block';
+            document.getElementById(id).style.opacity = '100';
+        }, 500);
+    }
+
 }
 
 
