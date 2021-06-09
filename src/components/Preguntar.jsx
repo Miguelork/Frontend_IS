4a5950e83b1779cc9358728cf976ac64624becfa
Mariavalen <66182385+Mariavalen@users.noreply.github.com>
2021-06-08 22:29:07 -0400

    Arreglando problemas con el cargando

--- src/components/Preguntar.jsx
+++ src/components/Preguntar.jsx
@@ -40,13 +40,19 @@ class Preguntar extends React.Component {
                             <div className="section-title" data-aos="fade-up">
                                 <h2>Realizar una pregunta</h2>
                             </div>
-                            <div className="row">
+                            <div className="row section-title" data-aos="fade-left" id="cargando" style={{ "display": "none" }}>
+                                <div className="lds-spinner" style={{ "padding-right": "90px" }} ><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /></div>
+                                <p style={{ "color": "white", "font-size": "12px" }}>Cargando</p>
+                                <style dangerouslySetInnerHTML={{ __html: "\n.lds-spinner {\n  color: official;\n  display: inline-block;\n  position: relative;\n  width: 80px;\n  height: 80px;\n}\n.lds-spinner div {\n  transform-origin: 40px 40px;\n  animation: lds-spinner 1.2s linear infinite;\n}\n.lds-spinner div:after {\n  content: \" \";\n  display: block;\n  position: absolute;\n  top: 3px;\n  left: 37px;\n  width: 6px;\n  height: 18px;\n  border-radius: 20%;\n  background: #fff;\n}\n.lds-spinner div:nth-child(1) {\n  transform: rotate(0deg);\n  animation-delay: -1.1s;\n}\n.lds-spinner div:nth-child(2) {\n  transform: rotate(30deg);\n  animation-delay: -1s;\n}\n.lds-spinner div:nth-child(3) {\n  transform: rotate(60deg);\n  animation-delay: -0.9s;\n}\n.lds-spinner div:nth-child(4) {\n  transform: rotate(90deg);\n  animation-delay: -0.8s;\n}\n.lds-spinner div:nth-child(5) {\n  transform: rotate(120deg);\n  animation-delay: -0.7s;\n}\n.lds-spinner div:nth-child(6) {\n  transform: rotate(150deg);\n  animation-delay: -0.6s;\n}\n.lds-spinner div:nth-child(7) {\n  transform: rotate(180deg);\n  animation-delay: -0.5s;\n}\n.lds-spinner div:nth-child(8) {\n  transform: rotate(210deg);\n  animation-delay: -0.4s;\n}\n.lds-spinner div:nth-child(9) {\n  transform: rotate(240deg);\n  animation-delay: -0.3s;\n}\n.lds-spinner div:nth-child(10) {\n  transform: rotate(270deg);\n  animation-delay: -0.2s;\n}\n.lds-spinner div:nth-child(11) {\n  transform: rotate(300deg);\n  animation-delay: -0.1s;\n}\n.lds-spinner div:nth-child(12) {\n  transform: rotate(330deg);\n  animation-delay: 0s;\n}\n@keyframes lds-spinner {\n  0% {\n    opacity: 1;\n  }\n  100% {\n    opacity: 0;\n  }\n}\n" }} />
+                            </div>
+                            <div className="row" id="pregunta" >
                                 <div className="col-md-4 hero-img">
                                     <img src="assets/img/foroimg.svg" className="img-fluid animated" alt="" />
                                 </div>
                                 <div className="col-md-1"></div>
                                 <div className="col-md-7" data-aos="fade-left" id="DocFormulario">
-                                    <form role="form" id="pregunta" class="php-email-form" style={{ "width": "100%" }}>
+
+                                    <form role="form"  class="php-email-form" style={{ "width": "100%" }}>
                                         <div class="form-group">
                                             <h2 className="mt-4 mb-4" style={{ "color": "white" }}><b>Ingresa aqui tu duda</b></h2>
                                             <input type="text" id="titulo" name="titulo" className="form-control" placeholder="Escriba su duda aqui " required />
@@ -92,7 +98,8 @@ async function crearPregunta() {
     ocultar('pregunta');
     mostrar('cargando');
     // Envio POST al backend
-    axios.post('https://dblinkmed.herokuapp.com/crearPregunta', {
+    
+    axios.post('https://dblinkmed.herokuapp.com/crearPregunta', { 
         titulo: Pregunta.titulo,
         descripcion: Pregunta.descripcion,
         nombre: Pregunta.nombre,
