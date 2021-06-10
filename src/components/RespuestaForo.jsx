db0b73020f71e70c92c28ebe23bdb31b93718734
Mariavalen <66182385+Mariavalen@users.noreply.github.com>
2021-06-10 19:53:45 -0400

    Ya se pueden agregar respuestas a las preguntas en el foro

--- src/components/RespuestaForo.jsx
+++ src/components/RespuestaForo.jsx
@@ -99,6 +99,11 @@ class RespuestaForo extends React.Component {
                                 <p style={{ "color": "white", "font-size": "14px" }}>{this.state.data.descripcion}</p>
                                 <input type="hidden" value={this.state.data._id} id="idPregunta" /> 
                             </div>
+                            <div className="row section-title" data-aos="fade-left" id="cargando" style={{ "display": "none" }}>
+                                <div className="lds-spinner" style={{ "padding-right": "90px" }} ><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /></div>
+                                <p style={{ "color": "white", "font-size": "12px" }}>Cargando</p>
+                                <style dangerouslySetInnerHTML={{ __html: "\n.lds-spinner {\n  color: official;\n  display: inline-block;\n  position: relative;\n  width: 80px;\n  height: 80px;\n}\n.lds-spinner div {\n  transform-origin: 40px 40px;\n  animation: lds-spinner 1.2s linear infinite;\n}\n.lds-spinner div:after {\n  content: \" \";\n  display: block;\n  position: absolute;\n  top: 3px;\n  left: 37px;\n  width: 6px;\n  height: 18px;\n  border-radius: 20%;\n  background: #fff;\n}\n.lds-spinner div:nth-child(1) {\n  transform: rotate(0deg);\n  animation-delay: -1.1s;\n}\n.lds-spinner div:nth-child(2) {\n  transform: rotate(30deg);\n  animation-delay: -1s;\n}\n.lds-spinner div:nth-child(3) {\n  transform: rotate(60deg);\n  animation-delay: -0.9s;\n}\n.lds-spinner div:nth-child(4) {\n  transform: rotate(90deg);\n  animation-delay: -0.8s;\n}\n.lds-spinner div:nth-child(5) {\n  transform: rotate(120deg);\n  animation-delay: -0.7s;\n}\n.lds-spinner div:nth-child(6) {\n  transform: rotate(150deg);\n  animation-delay: -0.6s;\n}\n.lds-spinner div:nth-child(7) {\n  transform: rotate(180deg);\n  animation-delay: -0.5s;\n}\n.lds-spinner div:nth-child(8) {\n  transform: rotate(210deg);\n  animation-delay: -0.4s;\n}\n.lds-spinner div:nth-child(9) {\n  transform: rotate(240deg);\n  animation-delay: -0.3s;\n}\n.lds-spinner div:nth-child(10) {\n  transform: rotate(270deg);\n  animation-delay: -0.2s;\n}\n.lds-spinner div:nth-child(11) {\n  transform: rotate(300deg);\n  animation-delay: -0.1s;\n}\n.lds-spinner div:nth-child(12) {\n  transform: rotate(330deg);\n  animation-delay: 0s;\n}\n@keyframes lds-spinner {\n  0% {\n    opacity: 1;\n  }\n  100% {\n    opacity: 0;\n  }\n}\n" }} />
+                            </div>
                             <div className="row" data-aos="fade-left">
                                 <section id="faq" className="faq section-bg" style={{ "background": "none", "padding-top": "0rem" }}>
                                     <div className="faq-list">
@@ -137,7 +142,39 @@ function agregarRespuesta (){
     Respuesta.apellido = usuario.apellido; // Poniendo al objeto pregunta el apellido
     Respuesta.user = usuario.user; // Poniendo la objeto pregunta el user
     console.log(Respuesta)
-    
+    ocultar('faq');
+    mostrar('cargando');
+        axios.post('https://dblinkmed.herokuapp.com/crearRespuesta', { 
+        idPregunta: Respuesta.idPregunta,
+        respuesta: Respuesta.respuesta,
+        nombre: Respuesta.nombre,
+        apellido: Respuesta.apellido,
+        user: Respuesta.user
+    })
+        .then(function (response) {
+            console.log(response);
+            setTimeout(() => { window.location.href = `/${Respuesta.idPregunta}`; }, 3000); // Re
+        })
+        .catch(function (error) {
+            console.log(error);
+            ocultar('cargando');
+            mostrar('faq');
+        });
+    }
 
-}
+    function ocultar(id) {
+        document.getElementById(id).style.opacity = '0';
+        document.getElementById(id).style.transition = 'opacity 0.5s';
+        setTimeout(() => { document.getElementById(id).style.display = 'none'; }, 500);
+    }
+
+    function mostrar(id) {
+        setTimeout(() => {
+            document.getElementById(id).style.display = 'block';
+            document.getElementById(id).style.opacity = '100';
+        }, 500);
+    }
+
+
+
 export default withRouter(RespuestaForo);
