1aba035e89bccc001d72768969f5df5fb62f3611
Manuel_Rodriguez <manuelrodriguez@correo.unimet.edu.ve>
2021-07-03 14:08:41 -0400

    Documentando el proyecto

--- src/components/Preguntar.jsx
+++ src/components/Preguntar.jsx
@@ -38,13 +38,6 @@ class Preguntar extends React.Component {
     }
 
 
-    componentWillUnmount() {
-        // fix Warning: Can't perform a React state update on an unmounted component
-        this.setState = (state,callback)=>{
-            return;
-        };
-    }
-
     render() {
         return (
             <div data-testid="Preguntar">
